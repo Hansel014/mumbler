0f955164a1d8f520f9c4ca834374a9ef3cc2a47c
messerr <dmesser@me.com>
2015-02-05 15:34:58 -0500

    Added followers capability

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,15 @@ Rails.application.routes.draw do
     resources :posts do 
       resources :comments
     end
-    resources :relationships, only: [:create, :destroy]
+    # resources :relationships, only: [:create, :destroy]
+
+    get '/follow/:id', to: 'relationships#create', as: 'follow'
+    get '/unfollow/:id', to: 'relationships#destroy', as: 'unfollow'
 
      get '/signin', to: 'sessions#new'
      post '/signin', to: 'sessions#create'
      delete '/logout', to: 'sessions#destroy'
+
 
   # You can have the root of your site routed with "root"
   root 'users#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150205170254) do
-
 
   create_table "comments", force: :cascade do |t|
     t.string   "title"
